f93bae6dfbd52d8dc34431d278f09b2be13f52b1
Lena-Grace <lena.grace@cincycircle.com>
2019-01-02 16:41:31 -0500

    new python test file

---
+++ python/test.py
@@ -0,0 +1,18 @@
+str = "Hello World"
+
+for x in str:
+    print(x)
+
+t = [1, 2, 3, 4, 's', 5]
+
+for x in t:
+    print(x)
+
+
+def EPrint(list):
+    for x in list:
+        print(x)
+
+list = "Stuff and Things"
+
+EPrint(list)
